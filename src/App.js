c763e658cc58e062a8523e03f69b91cd7d74d69c
Stas <46508483+Stas9iMba@users.noreply.github.com>
2021-06-25 03:17:06 +0700

    fix images

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
+import logo from './images/icons/logo.png';
+import cart from './images/icons/cart.svg';
+import user from './images/icons/user.svg';
+import btn_add from './images/icons/button-add.svg';
+import product_1 from './images/product/product-1.jpg';
+import product_2 from './images/product/product-2.jpg';
+import product_3 from './images/product/product-3.jpg';
+import product_4 from './images/product/product-4.jpg';
+
 function App() {
   return (
     <div className="wrapper">
       <div className="container">
-        <header className='header'>
+        <header className="header">
           <div className="header__logo">
-            <img width={40} height={40} src="/images/icons/logo.png" alt="" />
+            <img width={40} height={40} src={logo} alt="logo" />
             <div>
               <h3 className="header__title">REACT Shop</h3>
-              <p  className="header__subtitle">Магазин лучших кроссовок</p>
+              <p className="header__subtitle">Магазин лучших кроссовок</p>
             </div>
           </div>
           <ul className="header__list">
             <li className="header__item">
-              <img width={18} height={18} src="/images/icons/cart.svg" alt="" />
-              <span>1205 руб.</span>
+              <img width={18} height={18} src={cart} alt="" />
+              <span>12 999 руб.</span>
             </li>
             <li className="header__item">
               <svg
@@ -29,16 +38,34 @@ function App() {
               </svg>
             </li>
             <li className="header__item">
-              <img width={20} height={20} src="/images/icons/user.svg" alt="" />
+              <img width={20} height={20} src={user} alt="" />
             </li>
           </ul>
         </header>
-        
+
         <div className="content">
-          <h1 className="content__title">Все кросcовки</h1>
+          <div className="content__top">
+            <h1 className="content__title">Все кросcовки</h1>
+            <form action="" className="content__form">
+              <input className="content__form-input" type="text" placeholder="Поиск..." />
+            </form>
+          </div>
           <div className="content__inner">
             <div className="card">
-              <img width={133} height={112} src="/images/product/product-1.jpg" alt="" />
+              <button className="card__btn card__btn--heard">
+                <svg
+                  width="15"
+                  height="14"
+                  viewBox="0 0 15 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M14.5849 3.22311C14.3615 2.7098 14.0394 2.24464 13.6365 1.85368C13.2333 1.46155 12.758 1.14993 12.2363 0.935761C11.6954 0.712803 11.1152 0.59868 10.5295 0.600018C9.70772 0.600018 8.90596 0.823295 8.20921 1.24504C8.04253 1.34593 7.88418 1.45674 7.73416 1.57748C7.58414 1.45674 7.42579 1.34593 7.2591 1.24504C6.56236 0.823295 5.7606 0.600018 4.93884 0.600018C4.3471 0.600018 3.7737 0.712483 3.23198 0.935761C2.70858 1.15077 2.23686 1.46005 1.83181 1.85368C1.42843 2.2442 1.10619 2.70947 0.883373 3.22311C0.65168 3.75732 0.533333 4.32461 0.533333 4.90844C0.533333 5.45919 0.646679 6.0331 0.871705 6.61693C1.06006 7.10483 1.33009 7.61092 1.67513 8.12198C2.22186 8.93074 2.97361 9.77423 3.90705 10.6293C5.4539 12.0467 6.98574 13.0258 7.05075 13.0655L7.44579 13.3169C7.62081 13.4277 7.84584 13.4277 8.02086 13.3169L8.4159 13.0655C8.48091 13.0242 10.0111 12.0467 11.5596 10.6293C12.493 9.77423 13.2448 8.93074 13.7915 8.12198C14.1366 7.61092 14.4083 7.10483 14.5949 6.61693C14.82 6.0331 14.9333 5.45919 14.9333 4.90844C14.935 4.32461 14.8166 3.75732 14.5849 3.22311Z"
+                    fill="#D3D3D3"
+                  />
+                </svg>
+              </button>
+              <img width={133} height={112} src={product_1} alt="" />
               <h5 className="card__name">Мужские Кроссовки Nike Blazer Mid Suede</h5>
               <div className="card__info">
                 <div className="card__info-price">
@@ -46,12 +73,12 @@ function App() {
                   <b>12 999 руб.</b>
                 </div>
                 <button className="card__btn">
-                  <img width={11} height={11} src="/images/icons/button-add.svg" alt="" />
+                  <img width={11} height={11} src={btn_add} alt="" />
                 </button>
               </div>
             </div>
             <div className="card">
-              <img width={133} height={112} src="/images/product/product-2.jpg" alt="" />
+              <img width={133} height={112} src={product_2} alt="" />
               <h5 className="card__name">Мужские Кроссовки Nike Blazer Mid Suede</h5>
               <div className="card__info">
                 <div className="card__info-price">
@@ -59,12 +86,12 @@ function App() {
                   <b>12 999 руб.</b>
                 </div>
                 <button className="card__btn">
-                  <img width={11} height={11} src="/images/icons/button-add.svg" alt="" />
+                  <img width={11} height={11} src={btn_add} alt="" />
                 </button>
               </div>
             </div>
             <div className="card">
-              <img width={133} height={112} src="/images/product/product-3.jpg" alt="" />
+              <img width={133} height={112} src={product_3} alt="" />
               <h5 className="card__name">Мужские Кроссовки Nike Blazer Mid Suede</h5>
               <div className="card__info">
                 <div className="card__info-price">
@@ -72,12 +99,12 @@ function App() {
                   <b>12 999 руб.</b>
                 </div>
                 <button className="card__btn">
-                  <img width={11} height={11} src="/images/icons/button-add.svg" alt="" />
+                  <img width={11} height={11} src={btn_add} alt="" />
                 </button>
               </div>
             </div>
             <div className="card">
-              <img width={133} height={112} src="/images/product/product-4.jpg" alt="" />
+              <img width={133} height={112} src={product_4} alt="" />
               <h5 className="card__name">Мужские Кроссовки Nike Blazer Mid Suede</h5>
               <div className="card__info">
                 <div className="card__info-price">
@@ -85,7 +112,7 @@ function App() {
                   <b>12 999 руб.</b>
                 </div>
                 <button className="card__btn">
-                  <img width={11} height={11} src="/images/icons/button-add.svg" alt="" />
+                  <img width={11} height={11} src={btn_add} alt="" />
                 </button>
               </div>
             </div>
